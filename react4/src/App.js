2f705cbb51268b73e481e9ee4a0d156cce379e12
Seo-de <110797113+Seo-de@users.noreply.github.com>
2023-05-25 17:33:17 +0900

    react5

--- react4/src/App.js
+++ react4/src/App.js
@@ -1,41 +1,38 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function byFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)");
-    // 자주 사용하지는 않는대.
-    return byFn;
-  }
-  // useEffect(hiFn, []);
-
-  // 위의 코드를 다르게 작성해본 코드
-  // 방법 1.
-  useEffect(() => {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-
-  // 방법 2.
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
+import { useState } from "react";
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    /* 이 state는 항상 새 []여야 한대 
+      '...배열'을 써주면 배열의 있는 모든 element들을 하나씩 꺼내온대*/
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      {/* showing이면 <hello />를 보여주고 아니면 null */}
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {/* toDos를 arr에 있는 아이템을 내가 원하는 값으로 바꿔서 map에 넣는다 */}
+        {toDos.map((item, index) => (
+          /* map에 넣었기 때문에 key값이 필요해서 index 값을 넣어줌 */
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
